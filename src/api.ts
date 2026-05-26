d950c5362e1ad6137a27d8d5ed8f051f0fb3c05e
Marcmc1803 <marc.martin.cuartero@estudiantat.upc.edu>
2026-05-26 12:21:50 +0200

    fix: update frontend API URL port to 1337 to match local backend port

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:9000";
+const API_URL = "http://localhost:1337";
 //const API_URL = "https://ea3-api.upc.edu";
 const api = axios.create({
   baseURL: API_URL,
