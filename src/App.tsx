d5f7d95900e3bf7dab9caac21a83e40d7548826e
VictorPeso <victor.peso@gmail.com>
2026-06-21 11:41:26 +0200

    v1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Routes, Route, Link, useLocation, Navigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
